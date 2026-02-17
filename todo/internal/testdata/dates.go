89c66e88b3bc35ee9a1d095b678ab3589ce5ce4b
KEINOS <github@keinos.com>
2026-02-17 09:45:31 +0900

    chore: add more tests

--- todo/internal/testdata/dates.go
+++ todo/internal/testdata/dates.go
@@ -10,10 +10,10 @@ var Dates = []struct {
 	// Golden test cases
 	{"golden case", "2024-01-01", true, true},
 	{"golden case", "2024-12-31", true, true},
-	{"leap year", "2024-02-29", true, true},
-	{"old date", "1234-06-15", true, true},
 
 	// Edge cases
+	{"old date", "1234-06-15", true, true},
+	{"leap year", "2024-02-29", true, true},
 	{"earliest possible date", "0000-01-01", true, true},
 	{"far future date", "9999-12-31", true, true},
 	{"valid format but invalid date", "2024-99-99", true, false},
@@ -23,6 +23,7 @@ var Dates = []struct {
 	{"common but invalid (not supported)", "20240101", false, false},
 	{"common but invalid (short year not supported)", "24-01-01", false, false},
 	{"XXXX-XX-XX format but not a date", "date-is-10", false, false},
+	{"XXXX-XX-XX format but in full-width characters", "２０２４－０１－０１", false, false},
 	{"completely invalid", "hello-world", false, false},
 	{"wrong separator", "2024/01/01", false, false},
 	{"missing leading zeros", "2024-1-1", false, false},
